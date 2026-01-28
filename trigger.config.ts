6f169e1cc2f4725a277a77c23a3ced911405f350
Nitish Kumar <nknitish90062@gmail.com>
2026-01-28 20:25:26 +0530

    fix error

--- trigger.config.ts
+++ trigger.config.ts
@@ -1,7 +1,11 @@
 import { defineConfig } from "@trigger.dev/sdk/v3";
 
+if (!process.env.TRIGGER_PROJECT_ID) {
+  throw new Error("Missing TRIGGER_PROJECT_ID");
+}
+
 export default defineConfig({
-  project: process.env.TRIGGER_PROJECT_ID || "proj_dcvzlrezrgwmamipmkam", 
+  project: process.env.TRIGGER_PROJECT_ID, 
   runtime: "node",
   logLevel: "info",
   maxDuration: 300,
